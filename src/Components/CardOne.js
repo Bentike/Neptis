ed1eb22b55fca927d74c176831454d96f155e30d
bentike <ajadiyusuf73@gmail.com>
2023-06-27 18:55:20 +0100

    added react-icon package

---
+++ src/Components/CardOne.js
@@ -0,0 +1,11 @@
+const CardOne = () => {
+    return(
+        <div>
+           <p>Free</p>
+           <p>0$</p>
+           <button>Get Started</button>
+        </div>
+    );
+}
+
+export default CardOne;
