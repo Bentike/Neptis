9c8896f94191d0d3335325e4569ec33bc94adbf7
bentike <ajadiyusuf73@gmail.com>
2023-06-27 14:32:39 +0100

    added Features section

---
+++ src/Components/Features.js
@@ -0,0 +1,13 @@
+import FeaturesCard from "./FeaturesCard";
+const Features = () => {
+    return(
+        <div>
+           <FeaturesCard title="AI-Powered Website Generation" text="Create stunning and functional websites effortlessly with our AI-powered website generation feature, which seamlessly transforms web designs into fully operational sites."/>
+           <FeaturesCard title="Customizable Design Elements" text="Customize every aspect of your website with ease using our wide range of design elements, ensuring a unique and personalized online presence."/>
+           <FeaturesCard title="User-Friendly Interface" text="Creating a website has never been easier. Our user-friendly interface ensures a seamless and intuitive website building experience."/>
+           <FeaturesCard title="Responsive Website Outputs" text="Our platform automatically adapts your website's design and layout to different screen sizes, providing optimal viewing experiences on desktops, laptops, tablets, and smartphones."/>
+        </div>
+    );
+}
+
+export default Features;
---
+++ src/Components/FeaturesCard.js
@@ -0,0 +1,10 @@
+const FeaturesCard = ({title, text}) => {
+   return(
+      <div>
+         <h4>{title}</h4>
+         <p>{text}</p>
+      </div>
+   );
+}
+
+export default FeaturesCard;
