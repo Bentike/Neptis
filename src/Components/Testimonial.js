8e77696c32e235d790298dee1080f7e733e82c8a
bentike <ajadiyusuf73@gmail.com>
2023-06-28 14:05:50 +0100

    hide indicators on carousel

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -2,36 +2,84 @@ import "../Styles/testimonial.css";
 import client from "../images/testimonial-image.png";
 import arrowBack from "../images/arrow-back.png";
 import arrowForward from "../images/arrow-forward.png";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Testimonial = () => {
   return (
     <div className="testimonial">
       <h3>Testimonials</h3>
-      <div>
-        <div className="testimonial-text-wrap">
-          <p className="testimonial-text">
-            Lorem ipsum dolor sit amet consectetur. Aliquet scelerisque sem
-            commodo turpis ipsum. Et tristique arcu arcu sed feugiat tempus
-            purus nullam gravida. Vitae gravida dui gravida amet vitae.
-            Malesuada quisque accumsan quis cursus enim. Commodo in sit platea
-            eget lobortis. Suspendisse nulla leo eget fringilla justo ultrices.
-            Vestibulum sed mi donec maecenas sit nisi. Etiam tincidunt ultrices
-            sit sit sociis rutrum dui. Fames nunc quam ullamcorper cursus. Diam
-            vitae euismod curabitur ullamcorper.
-          </p>
+      <Carousel showIndicators={false}>
+        <div>
+          <div className="testimonial-text-wrap">
+            <p className="testimonial-text">
+              Lorem ipsum dolor sit amet consectetur. Aliquet scelerisque sem
+              commodo turpis ipsum. Et tristique arcu arcu sed feugiat tempus
+              purus nullam gravida. Vitae gravida dui gravida amet vitae.
+              Malesuada quisque accumsan quis cursus enim. Commodo in sit platea
+              eget lobortis. Suspendisse nulla leo eget fringilla justo
+              ultrices. Vestibulum sed mi donec maecenas sit nisi. Etiam
+              tincidunt ultrices sit sit sociis rutrum dui. Fames nunc quam
+              ullamcorper cursus. Diam vitae euismod curabitur ullamcorper.
+            </p>
 
-          <div className="testimonial-details">
-            <img src={client} alt="client testimonial" className="text-img" />
-            <div>
-              <p className="name">Nicole Jaymer</p>
-              <p className="role">Head of Noin Group</p>
+            <div className="testimonial-details">
+              <img src={client} alt="client testimonial" className="text-img" />
+              <div>
+                <p className="name">Nicole Jaymer</p>
+                <p className="role">Head of Noin Group</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <div>
+          <div className="testimonial-text-wrap">
+            <p className="testimonial-text">
+              Lorem ipsum dolor sit amet consectetur. Aliquet scelerisque sem
+              commodo turpis ipsum. Et tristique arcu arcu sed feugiat tempus
+              purus nullam gravida. Vitae gravida dui gravida amet vitae.
+              Malesuada quisque accumsan quis cursus enim. Commodo in sit platea
+              eget lobortis. Suspendisse nulla leo eget fringilla justo
+              ultrices. Vestibulum sed mi donec maecenas sit nisi. Etiam
+              tincidunt ultrices sit sit sociis rutrum dui. Fames nunc quam
+              ullamcorper cursus. Diam vitae euismod curabitur ullamcorper.
+            </p>
+
+            <div className="testimonial-details">
+              <img src={client} alt="client testimonial" className="text-img" />
+              <div>
+                <p className="name">Nicole Jaymer</p>
+                <p className="role">Head of Noin Group</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="testimonial-text-wrap">
+            <p className="testimonial-text">
+              Lorem ipsum dolor sit amet consectetur. Aliquet scelerisque sem
+              commodo turpis ipsum. Et tristique arcu arcu sed feugiat tempus
+              purus nullam gravida. Vitae gravida dui gravida amet vitae.
+              Malesuada quisque accumsan quis cursus enim. Commodo in sit platea
+              eget lobortis. Suspendisse nulla leo eget fringilla justo
+              ultrices. Vestibulum sed mi donec maecenas sit nisi. Etiam
+              tincidunt ultrices sit sit sociis rutrum dui. Fames nunc quam
+              ullamcorper cursus. Diam vitae euismod curabitur ullamcorper.
+            </p>
+
+            <div className="testimonial-details">
+              <img src={client} alt="client testimonial" className="text-img" />
+              <div>
+                <p className="name">Nicole Jaymer</p>
+                <p className="role">Head of Noin Group</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Carousel>
       <div className="controls">
-          <img src={arrowBack} alt="slide back"/>          
-          <img src={arrowForward} alt="slide forward" className="forward"/>          
+        <img src={arrowBack} alt="slide back" />
+        <img src={arrowForward} alt="slide forward" className="forward" />
       </div>
     </div>
   );
