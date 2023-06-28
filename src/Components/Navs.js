9722aedf5ad5863763881f07f5837237af4421ea
bentike <ajadiyusuf73@gmail.com>
2023-06-28 02:14:48 +0100

    added overlay nav

--- src/Components/Navs.js
+++ src/Components/Navs.js
@@ -2,18 +2,31 @@ import "../Styles/navs.css";
 import hambugger from "../images/hambugger.png";
 
 const Navs = () => {
-    return(
-       <div>
-           <ul className="nav-items">
-              <li>Home</li>
-              <li>Features</li>
-              <li>How it works</li>
-              <li>Pricing</li>
-              <li><button>Get Started</button></li>
-           </ul>
-           <img src={hambugger} alt="open nav-links" className="hambugger"/>
-       </div>
-    );
-}
+  return (
+    <div>
+      <ul className="nav-items">
+        <li>Home</li>
+        <li>Features</li>
+        <li>How it works</li>
+        <li>Pricing</li>
+        <li>
+          <button>Get Started</button>
+        </li>
+      </ul>
+
+      <img src={hambugger} alt="open nav-links" className="hambugger" />
+
+      <div className="nav-overlay">
+        <ul className="overlay-list">
+          <p className="close">X</p>
+          <li>Home</li>
+          <li>Features</li>
+          <li>How it works</li>
+          <li>Pricing</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navs;
